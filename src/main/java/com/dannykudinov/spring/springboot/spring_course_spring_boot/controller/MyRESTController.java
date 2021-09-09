787e8470ede435340ab7ac29f9cb97e61519da60
Denis <danny.wasth@gmail.com>
2021-09-09 10:44:58 +0300

    Cобрал проект, подключил к БД

---
+++ src/main/java/com/dannykudinov/spring/springboot/spring_course_spring_boot/controller/MyRESTController.java
@@ -0,0 +1,51 @@
+package com.dannykudinov.spring.springboot.spring_course_spring_boot.controller;
+
+import com.dannykudinov.spring.springboot.spring_course_spring_boot.entity.Employee;
+import com.dannykudinov.spring.springboot.spring_course_spring_boot.service.EmployeeService;
+import com.dannykudinov.spring.springboot.spring_course_spring_boot.service.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService = new EmployeeServiceImpl();
+
+    @GetMapping("/employees")
+    public List<Employee> showEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+//    @GetMapping("/employees/{id}")
+//    public Employee getEmployee(@PathVariable int id) {
+//        Employee employee = employeeService.getEmployee(id);
+//
+//        return employee;
+//    }
+//
+//    @PostMapping("/employees")
+//    public Employee postEmployee(@RequestBody Employee employee) {
+//        employeeService.saveEmployee(employee);
+//        return employee;
+//    }
+//
+//    @PutMapping("/employees")
+//    public Employee updateEmployee(@RequestBody Employee employee) {
+//        employeeService.saveEmployee(employee);
+//        return employee;
+//    }
+//
+//    @DeleteMapping("/employees/{id}")
+//    public String deleteEmployee(@PathVariable int id) {
+//        Employee employee = employeeService.getEmployee(id);
+//        employeeService.deleteEmployee(id);
+//        return "Employee with ID = " + id + " was deleted.";
+//    }
+}
---
+++ src/main/java/com/dannykudinov/spring/springboot/spring_course_spring_boot/service/EmployeeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.dannykudinov.spring.springboot.spring_course_spring_boot.service;
+
+import com.dannykudinov.spring.springboot.spring_course_spring_boot.dao.EmployeeDAO;
+import com.dannykudinov.spring.springboot.spring_course_spring_boot.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        List<Employee> empList = employeeDAO.getAllEmployees();
+        return empList;
+    }
+
+//    @Override
+//    @Transactional
+//    public void saveEmployee(Employee employee) {
+//        employeeDAO.saveOrUpdateEmployee(employee);
+//    }
+//
+//    @Override
+//    @Transactional
+//    public Employee getEmployee(int id) {
+//        Employee employee = employeeDAO.getEmployee(id);
+//        return employee;
+//    }
+//
+//    @Override
+//    @Transactional
+//    public void deleteEmployee(int id) {
+//        employeeDAO.deleteEmployee(id);
+//    }
+}
